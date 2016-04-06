1719763dd4fc41b74c4d3a0aec8e5622b9c193e8
Niall McGrath <mcgrath94@hotmail.com>
2016-04-06 01:10:45 +0100

    Delete Traversator.java

--- AI-FuzzyMaze/src/ie/gmit/sw/ai/traversers/Traversator.java
+++
@@ -1,9 +0,0 @@
-package ie.gmit.sw.ai.traversers;
-
-import java.awt.*;
-import ie.gmit.sw.ai.*;
-
-public interface Traversator {
-	public void traverse(Node[][] maze, Node start, Component viewer);
-
-}
